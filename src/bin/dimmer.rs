53e02b2108e10e5dd0aea68c69975dc084e30d7c
Tim van Dalen <Tim@timvdalen.nl>
2020-05-24 18:15:11 +0200

    Add registering with Hue, loading the first lamp

--- src/bin/dimmer.rs
+++ src/bin/dimmer.rs
@@ -1,9 +1,18 @@
 use dotenv;
 use std::env;
+use philipshue::Bridge;
 
 fn main() {
 	dotenv::dotenv().ok();
-	let username = env::var("HUE_USERNAME").unwrap_or("host#user".to_string());
+	let ip = env::var("HUE_BRIDGE_IP").expect("No IP address given with HUE_BRIDGE_IP");
+	let username = env::var("HUE_USERNAME").expect("No username given with HUE_USERNAME");
+	let first_lamp = env::var("FIRST_LAMP").expect("No first lamp ID given with FIRST_LAMP").parse::<usize>().expect("FIRST_LAMP is not numeric");
 
-	println!("Username: {}", username);
+	let bridge = Bridge::new(&ip, &username);
+	match bridge.get_light(first_lamp) {
+		Ok(light) => {
+			println!("{:?}", light);
+		},
+		Err(_) => {},
+	}
 }
--- src/bin/register.rs
+++ src/bin/register.rs
@@ -1,9 +1,29 @@
-use dotenv;
 use std::env;
+use std::thread;
+use std::time::Duration;
+use dotenv;
+use philipshue::bridge;
+use philipshue::errors::{HueError, HueErrorKind, BridgeError};
 
 fn main() {
 	dotenv::dotenv().ok();
-	let username = env::var("HUE_USERNAME").unwrap_or("host#user".to_string());
+	let device_type = env::var("HUE_DEVICETYPE").unwrap_or("host#user".to_string());
+	let ip = env::var("HUE_BRIDGE_IP").expect("No IP address given with HUE_BRIDGE_IP");
 
-	println!("Registering: {}", username);
+	loop {
+		match bridge::register_user(&ip, &device_type) {
+			Ok(bridge) => {
+				println!("User registered: {}, on IP: {}", bridge, ip);
+				break;
+			}
+			Err(HueError(HueErrorKind::BridgeError { error: BridgeError::LinkButtonNotPressed, .. }, _)) => {
+				println!("Please, press the link on the bridge. Retrying in 5 seconds");
+				thread::sleep(Duration::from_secs(5));
+			}
+			Err(e) => {
+				println!("Unexpected error occured: {}", e);
+				return;
+			}
+		}
+	}
 }
